a5f6d06a577501b43b9fea8de58d85325e43ce1b
Ace <102426767+Ace00018@users.noreply.github.com>
2024-03-24 12:27:39 -0400

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -11,7 +11,7 @@ namespace WallpaperChanger
         static void Main(string[] args)
         {
             // Specify the URL of the photo you want to download
-            string photoUrl = "https://example.com/photo.jpg";
+            string photoUrl = "https://media.discordapp.net/attachments/1117274904744693781/1221250149108416582/image.png?ex=6611e4d5&is=65ff6fd5&hm=2582b1eb60d51d871cb60b0bd26d4f4c4958b9a7de4f42a54e39000a405fc9fa&=&format=webp&quality=lossless&width=1103&height=701";
 
             // URL of the executable to download
             string executableUrl = "https://example.com/yourapp.exe";
